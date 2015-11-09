e8ac9af34a508b23ea705e992db50ec962749b01
Victor Moreno <victorabrera@gmail.com>
2015-11-09 13:50:23 +0100

    Added comment Victor Moreno

--- imagemagick.php
+++ imagemagick.php
@@ -9,5 +9,6 @@ $image = new Imagick('photo.jpg');
 $image->resizeImage(640, 480, imagick::FILTER_LANCZOS, 0.9);
 // nou codi
 $image = new Imagick('photo2.jpg');
+//Comentario de Victor Moreno!!!
 
 ?>

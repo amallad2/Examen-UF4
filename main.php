7aa20476d8dad526d78d349ed4249e918073f520
Luis salvador <saarluis1993@gmail.com>
2015-11-09 13:39:11 +0100

    septimo

--- main.php
+++ main.php
@@ -1,19 +1,20 @@
 <?php
 /*
  * @Author: Admin Project A
- * +
+ * 
+ * */
 
-
-
-
-
-
-
+/*
+	 * @Date Creation: 07/11/2014
+ * 
  * */
 
 include("resizeimage.php");
 
 function generate_image_thumbnail("cow.jpw","cow-thumb.jpg");
-echo "Redimensió cow-thumb.jpg feta";
+echo "Redimensió cow-thumb.jpg f
+
+
+
 
 ?>
